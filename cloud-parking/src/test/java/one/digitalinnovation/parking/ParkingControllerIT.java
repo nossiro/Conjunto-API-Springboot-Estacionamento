3c987bc51c59cdcf9737be92f277313b80c74e38
nossiro <orissongomes@gmail.com>
2022-11-16 15:08:38 -0500

    Mudanças

---
+++ cloud-parking/src/test/java/one/digitalinnovation/parking/ParkingControllerIT.java
@@ -0,0 +1,32 @@
+package one.digitalinnovation.parking;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import io.restassured.RestAssured;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ParkingControllerIT {
+
+    @LocalServerPort
+    private int randomPort;
+
+    @BeforeEach
+    public void setUpTest(){
+        System.out.println(randomPort);
+        RestAssured.port = randomPort;
+    }
+    
+    @Test
+    void findAll(){
+
+    }
+
+    @Test
+    void create(){
+
+    }
+
+}
